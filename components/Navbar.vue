5c7f760757697d9d6d044145bc760e0f5281e8bc
Gino Bartolucci <ginobartolucci00@gmail.com>
2022-06-07 01:11:13 -0300

    Componentes: navbar - eventsVerticalCard

---
+++ components/Navbar.vue
@@ -0,0 +1,79 @@
+   <template>
+  <div>
+    <!--Show mobile - Hide desktop -->
+    <v-navigation-drawer
+    class="d-flex d-sm-none"
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in btnItems"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action v-if="item.icon">
+            <v-icon left >{{ item.iconName }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!--Hide mobile - Show desktop -->
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <!--Show mobile - Hide desktop -->
+      <v-app-bar-nav-icon class="d-flex d-sm-none" @click.stop="drawer = !drawer" />
+      <!--Solo logeados-->
+      <div class="d-none d-sm-flex" v-for="(item,i) in btnItems" :key="i">
+        <v-btn plain text nuxt :to="item.to"><v-icon left v-if="item.icon">{{item.iconName}}</v-icon>{{item.title}}</v-btn>
+      </div>
+      
+      <v-spacer />
+      <v-btn plain text nuxt to="/">Salir
+        <v-icon >mdi-logout</v-icon> 
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      btnItems: [
+        {
+          icon:true,
+          iconName: 'mdi-home',
+          title: 'Inicio',
+          to: '/',
+        },      
+        {
+          icon:false,
+          iconName: 'mdi-apps',
+          title: 'Mis Comunidades',
+          to: '/communities',
+        },
+        {
+          icon:false,
+          iconName: 'mdi-chart-bubble',
+          title: 'Mis Eventos',
+          to: '/events',
+        },
+        ],
+      miniVariant: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
---
+++ components/events/verticalCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="mx-auto" min-width="280" max-width="400" hover="true">
+    <v-img class="white--text align-end" height="200px" :src="src">
+      <v-card-title>{{ title }}</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-1 text-left"> {{ date }} </v-card-subtitle>
+
+    <v-card-text class="text--primary text-left" >
+      <div class="pb-1">{{ place }}</div>
+
+      <div>{{ description }}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="orange" text> {{ community }} </v-btn>
+
+      <v-btn color="orange" text> <v-icon> mdi-heart-outline </v-icon></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<style scoped>
+
+</style>
+
+<script>
+export default {
+  props: {
+    title: String,
+    date: String,
+    place: String,
+    description: String,
+    community: String,
+    src: String,
+  },
+}
+</script>

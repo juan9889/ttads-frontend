78a118f758bbb0cc3a71eb89c2750f54dcf6593c
juan <juan.fogliato@gmail.com>
2022-06-08 20:32:57 -0300

    icono panel admin

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,12 +1,17 @@
 <template>
   <div >
+    <br />
+    <br />
+     <br />
+    <br />
     <v-container class="fill-height">
+      
     <v-row
         align="center"
         justify="center"
       >
       <v-col align="center">
-      <nuxt-logo></nuxt-logo>
+      <admin-panel-logo></admin-panel-logo>
       </v-col>
     </v-row>
     </v-container>
@@ -15,7 +20,9 @@
 
 
 <script>
+import AdminPanelLogo from '~/components/adminpanel/AdminPanelLogo.vue'
   export default {
+  components: { AdminPanelLogo },
     layout: 'adminLayout'
     // page component definitions
   }

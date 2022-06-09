bdf1e3fb4ff6a6445f7ce7317bf5ad1c29e2008a
juan <juan.fogliato@gmail.com>
2022-06-09 17:07:43 -0300

    abm ciudades

--- pages/admin_pages/abm_ciudades.vue
+++ pages/admin_pages/abm_ciudades.vue
@@ -1,22 +1,36 @@
 <template>
+<div>
+  <h1>Ciudades</h1>
+  <br />
+<v-btn
+  color="green"
+  elevation="24"
+>Agregar ciudad</v-btn>
+<br></br>
   <v-card>
     <v-card-title>
       Ciudades
       <v-spacer></v-spacer>
+     
+
       <v-text-field
         v-model="search"
         append-icon="mdi-magnify"
-        label="Search"
+        label="Buscar"
         single-line
         hide-details
       ></v-text-field>
     </v-card-title>
+    <v-spacer></v-spacer>
+     
+<v-spacer></v-spacer>
     <v-data-table
       :headers="headers"
-      :items="desserts"
+      :items="ciudades"
       :search="search"
     ></v-data-table>
   </v-card>
+  </div>
 </template>
 
 <script>
@@ -27,98 +41,27 @@
         search: '',
         headers: [
           {
-            text: 'Dessert (100g serving)',
+            text: 'Ciudad',
             align: 'start',
-            sortable: false,
+            sortable: true,
             value: 'name',
           },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' },
+          { text: 'Provincia', value: 'provincia' },
+          { text: 'ID', value: 'id' },
+          
         ],
-        desserts: [
+        ciudades: [
           {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
+            name: 'Rosario',
+            provincia: 'Santa Fe',
+            id : '1',
           },
           {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
+            name: 'Capitan Bermudez',
+            provincia: 'Santa Fe',
+            id : '2',
           },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
+          
         ],
       }
     },

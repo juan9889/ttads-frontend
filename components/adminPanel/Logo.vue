ceef0af2bae1594905e145445f8e0dba976d8c2b
juan <juan.fogliato@gmail.com>
2022-06-09 17:15:53 -0300

    quite href de logo panel admin

--- components/adminPanel/Logo.vue
+++ components/adminPanel/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <a title="Icons8, MIT &lt;http://opensource.org/licenses/mit-license.php&gt;, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Icons8_flat_settings.svg"><img width="256" alt="Icons8 flat settings" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Icons8_flat_settings.svg/512px-Icons8_flat_settings.svg.png"></a>
+  <a title="Icons8, MIT &lt;http://opensource.org/licenses/mit-license.php&gt;, via Wikimedia Commons"><img width="256" alt="Icons8 flat settings" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Icons8_flat_settings.svg/512px-Icons8_flat_settings.svg.png"></a>
 
 </template>
 

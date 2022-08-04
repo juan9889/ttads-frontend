bfa42375aea90a15b4a039e1276e728337301b26
Gino Bartolucci <ginobartolucci00@gmail.com>
2022-08-04 13:06:12 -0300

    explore

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,31 +1,16 @@
 <template>
   <div>
-    <v-app-bar 
-      app
-      clipped-right
-      flat
-      height="50"
-      color="tertiary"
-      >
+    <v-app-bar app clipped-right flat height="50" color="tertiary">
       <!--Solo logeados-->
-      <div class="d-none d-md-flex" v-for="(item,i) in btnItems" :key="i">
+      <div class="d-none d-md-flex" v-for="(item, i) in btnItems" :key="i">
         <v-btn plain text nuxt :to="item.to">
-          <v-icon left v-if="item.icon">{{item.iconName}}</v-icon>{{item.title}}
+          <v-icon left v-if="item.icon">{{ item.iconName }}</v-icon>{{ item.title }}
         </v-btn>
       </div>
-
       <v-spacer></v-spacer>
-
       <v-responsive max-width="300">
-        <v-text-field
-          dense
-          flat
-          hide-details
-          rounded
-          solo-inverted
-        ></v-text-field>
+        <v-text-field dense flat hide-details rounded solo-inverted></v-text-field>
       </v-responsive>
-      
       <v-spacer />
       <v-btn plain text nuxt to="/">Salir
         <v-icon>mdi-logout</v-icon>
@@ -44,18 +29,24 @@ export default {
       fixed: false,
       btnItems: [
         {
-          icon:true,
+          icon: true,
           iconName: 'mdi-home',
           title: 'Inicio',
           to: '/',
         },
         {
-          icon:false,
+          icon: false,
           iconName: 'mdi-chart-bubble',
           title: 'Administrador',
           to: '/admin',
         },
-        ],
+        {
+          icon: false,
+          iconName: 'mdi-chart-bubble',
+          title: 'Explorar',
+          to: '/explore',
+        }
+      ],
       miniVariant: false,
       title: 'Vuetify.js',
     }
--- pages/explore.vue
+++ pages/explore.vue
@@ -2,17 +2,17 @@
   <div>
     <h1 class="text-center mb-4">Comunidades</h1>
     <v-row justify="center">
-      <v-col cols="12" sm="6" xs="6" md="4" class="pt-3">
+      <v-col sm="3" xs="3" md="3" class="pt-3">
         <v-btn class="d-flex flex-column mb-2" v-for="category in categories" :key="category.id" elevation="6" raised>
           {{ category.name }}</v-btn>
       </v-col>
-      <v-col cols="12" sm="6" xs="6" md="8">
-        <div v-for="community in communities" :key="community.id">
-          <CommunitiesCard :name="community.name" :category="community.category" :description="community.description"
-            :src="'https://cdn.vuetifyjs.com/images/cards/cooking.png'"/>
-        </div>
+      <div v-for="community in communities" :key="community.id">
+      <v-col sm="3" xs="3" md="3">
+        <CommunitiesCard :name="community.name" :category="community.category" :description="community.description"
+          :src="'https://cdn.vuetifyjs.com/images/cards/cooking.png'" />
       </v-col>
-    </v-row>
+  </div>
+  </v-row>
   </div>
 </template>
 

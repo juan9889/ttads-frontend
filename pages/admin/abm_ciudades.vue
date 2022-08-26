95d1c88f6004cb88fa8a6b24572571d1588a2608
Juan Agustin Fogliato <48728949+juan9889@users.noreply.github.com>
2022-08-26 20:32:41 -0300

    abm ciudades ok

--- pages/admin/abm_ciudades.vue
+++ pages/admin/abm_ciudades.vue
@@ -22,10 +22,12 @@
       <v-spacer></v-spacer>
       <v-data-table :headers="headers" :items="ciudades" :search="search">
         <template v-slot:item.actions="{ item }">
-          <v-icon small class="mr-2" @click="editItem(item)">
+          <v-icon small class="mr-2" @click="open_edit_diag(item)">
             mdi-pencil
           </v-icon>
-          <v-icon small @click.stop="dialog_delete = true"> mdi-delete </v-icon>
+          <v-icon small @click.stop="open_delete_diag(item)">
+            mdi-delete
+          </v-icon>
         </template>
         <template v-slot:no-data> </template>
       </v-data-table>
@@ -40,9 +42,45 @@
           <v-btn color="default" text @click="dialog_delete = false">
             Cancelar
           </v-btn>
-          <v-btn color="red" fill @click="dialog_delete = false">
-            Eliminar
+          <v-btn color="red" fill @click="delete_confirm"> Eliminar </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialog_edit" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Editar ciudad</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="nuevo_nombre_edit_ciudad"
+                  label="Nombre"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-select
+                  :items="this.provincias"
+                  v-model="selected_province"
+                  label="Provincia"
+                  item-text="name"
+                  item-value="id"
+                  required
+                ></v-select>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="default" text @click="dialog_edit = false">
+            Cancelar
           </v-btn>
+          <v-btn color="green" filled @click="edit_confirm"> Guardar </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -56,12 +94,19 @@
           <v-container>
             <v-row>
               <v-col cols="12">
-                <v-text-field label="Nombre" required></v-text-field>
+                <v-text-field
+                  v-model="nombre_nueva_ciudad"
+                  label="Nombre"
+                  required
+                ></v-text-field>
               </v-col>
               <v-col cols="12">
                 <v-select
-                  :items="['Santa Fe', 'La pampa', 'Corrientes']"
+                  :items="this.provincias"
+                  v-model="selected_province"
                   label="Provincia"
+                  item-text="name"
+                  item-value="id"
                   required
                 ></v-select>
               </v-col>
@@ -73,12 +118,38 @@
           <v-btn color="default" text @click="dialog_new = false">
             Cancelar
           </v-btn>
-          <v-btn color="green" filled @click="dialog_new = false">
-            Guardar
-          </v-btn>
+          <v-btn color="green" filled @click="create"> Guardar </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-snackbar v-model="snackbar_success" right top color="green">
+      {{ this.snackbar_text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="snackbar_success = false"
+        >
+          x
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <v-snackbar v-model="snackbar_error" right top color="red">
+      {{ this.snackbar_text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="snackbar_error = false"
+        >
+          x
+        </v-btn>
+      </template>
+    </v-snackbar>
   </div>
 </template>
 
@@ -93,6 +164,16 @@ export default {
       dialog_edit: false,
       dialog_delete: false,
       setDialogConfirm: false, //true confirmado
+      snackbar_error: false,
+      snackbar_success: false,
+      snackbar_text: '',
+      item_borrar: '',
+      selectedItem: {
+        name: '',
+        provinceId: '',
+        id: 0,
+      },
+      editedIndex: -1,
 
       headers: [
         {
@@ -106,6 +187,10 @@ export default {
         { text: 'Acciones', value: 'actions', sortable: false },
       ],
       ciudades: [],
+      provincias: [],
+      nombre_nueva_ciudad: '',
+      nuevo_nombre_edit_ciudad: '',
+      selected_province: '',
     }
   },
   methods: {
@@ -113,16 +198,87 @@ export default {
       this.ciudades = []
       this.ciudades_req = await this.$axios.$get('cities')
       console.log(this.ciudades_req)
-      this.ciudades_req.forEach(async (ciudad) => {
-        var provincia_ciudad = await this.$axios.$get(
-          'provinces/' + ciudad.provinceId
-        )
-        ciudad.provincia = provincia_ciudad.name
+      this.ciudades_req.forEach((ciudad) => {
+        ciudad.provincia = this.getNombreProvinciaConID(ciudad.provinceId)
         this.ciudades.push(ciudad)
       })
     },
+    async getProvincias() {
+      this.provincias = await this.$axios.$get('provinces')
+      console.log(this.provincias)
+    },
+    getNombreProvinciaConID(id) {
+      var nombre = ''
+      this.provincias.forEach((provincia) => {
+        if (provincia.id == id) {
+          nombre = provincia.name
+        }
+      })
+      return nombre
+    },
+    async create() {
+      try {
+        await this.$axios.$post('cities', {
+          name: this.nombre_nueva_ciudad,
+          provinceId: this.selected_province,
+        })
+        this.dialog_new = false
+        this.snackbar_text = 'Ciudad creada exitosamente'
+        this.snackbar_success = true
+        await this.getCiudades()
+      } catch (e) {
+        console.log(e)
+        this.snackbar_text = 'Ocurrió un error al crear la ciudad'
+        this.snackbar_error = true
+      }
+    },
+    open_delete_diag(item) {
+      this.selectedItem = Object.assign({}, item)
+      console.log(this.selectedItem)
+      this.dialog_delete = true
+    },
+    open_edit_diag(item) {
+      this.selectedItem = Object.assign({}, item)
+      console.log(this.selectedItem)
+      this.nuevo_nombre_edit_ciudad = this.selectedItem.name
+      this.selected_province = this.selectedItem.provinceId
+      this.dialog_edit = true
+    },
+    async delete_confirm() {
+      try {
+        await this.$axios.$delete('cities/' + this.selectedItem.id)
+        this.dialog_delete = false
+        this.snackbar_success = false
+        this.snackbar_text = 'Se eliminó la ciudad correctamente'
+        this.snackbar_success = true
+        await this.getCiudades()
+      } catch (e) {
+        this.snackbar_text = 'Ocurrió un error al eliminar la ciudad'
+        this.snackbar_error = true
+        console.log(e)
+      }
+    },
+    async edit_confirm() {
+      try {
+        await this.$axios.$put('cities/' + this.selectedItem.id, {
+          name: this.nuevo_nombre_edit_ciudad,
+          provinceId: this.selected_province,
+        })
+        this.nuevo_nombre_edit_ciudad = ''
+        this.dialog_edit = false
+        this.snackbar_success = false
+        this.snackbar_text = 'Se modificó la ciudad'
+        this.snackbar_success = true
+        await this.getCiudades()
+      } catch (e) {
+        this.snackbar_text = 'Ocurrió un error al modificar la ciudad'
+        this.snackbar_error = true
+      }
+    },
   },
+
   async mounted() {
+    await this.getProvincias()
     await this.getCiudades()
   },
 }

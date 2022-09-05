2dd69ec1859be2275ef0cc7ccb24114266b63da6
Juan Agustin Fogliato <48728949+juan9889@users.noreply.github.com>
2022-09-04 23:41:40 -0300

    search page case insensitive

--- pages/search.vue
+++ pages/search.vue
@@ -2,14 +2,12 @@
   <div>
     <h1 class="mt-5 mb-3 text-center text-h2">Comunidades</h1>
     <v-text-field
-            label="Buscar"
-            placeholder="Buscar"
-            solo
-            v-model="srch_str"
-          ></v-text-field>
-          <v-btn color="green" elevation="24" @click.stop="filter()"
-      >Buscar</v-btn
-    >
+      label="Buscar"
+      placeholder="Buscar"
+      solo
+      v-model="srch_str"
+    ></v-text-field>
+    <v-btn color="green" elevation="24" @click.stop="filter()">Buscar</v-btn>
     <v-divider class="mt-10 mb-5"></v-divider>
     <v-row class="justify-space-around">
       <SkeletonCard v-if="loadingCommunities" :amount="6"></SkeletonCard>
@@ -37,30 +35,30 @@ export default {
       this.$axios
         .get('communities')
         .then((data) => {
-          this.communities = data.data;
-          this.shown=this.communities;
-          this.loadingCommunities = false;
+          this.communities = data.data
+          this.shown = this.communities
+          this.loadingCommunities = false
         })
         .catch((err) => {
           console.log(err)
         })
     },
     filter() {
-      console.log('reached filter func');
-      console.log(this.srch_str);
-      if(this.srch_str.length<1){
-        this.shown=this.communities;
-
-      }else{
-        this.shown=[];
+      if (this.srch_str.length < 1) {
+        this.shown = this.communities
+      } else {
+        this.shown = []
         this.communities.forEach((com) => {
-          if(com.name.includes(this.srch_str) || com.description.includes(this.srch_str)){
-            this.shown.push(com);
+          console.log(com)
+          if (
+            com.name.toLowerCase().includes(this.srch_str.toLowerCase()) ||
+            com.description.toLowerCase().includes(this.srch_str.toLowerCase())
+          ) {
+            this.shown.push(com)
           }
         })
       }
     },
-    },
-  }
-
+  },
+}
 </script>

8fb7af03f31e90daf7d3051477089a98542f719f
Cristian Gofiar <cristiangofiar@gmail.com>
2022-11-13 09:04:52 -0300

    footer grupo 10

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
   <v-footer app absolute height="50" inset>
     <span class="text-center text-h6">&copy; {{ new Date().getFullYear() }}</span>
+    <v-spacer></v-spacer>
+    <span class="text-right text-h6"> Grupo 10 </span>
   </v-footer>
 </template>
 

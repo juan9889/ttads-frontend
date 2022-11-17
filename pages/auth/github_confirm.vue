6383c8813a207f5be1f39a3e981b94173b51743d
Juan Agustin Fogliato <48728949+juan9889@users.noreply.github.com>
2022-11-17 19:29:31 -0300

    oauth servidor ok

--- pages/auth/github_confirm.vue
+++ pages/auth/github_confirm.vue
@@ -7,9 +7,64 @@
       <v-container class="fill-height">
         <v-row justify="center">
           <v-col>
-            OK
+            <v-container style="height: 400px;">
+      <v-row
+        class="fill-height"
+        align-content="center"
+        justify="center"
+      >
+        <v-col
+          class="text-subtitle-1 text-center"
+          cols="12"
+        >
+          Iniciando sesion...
+        </v-col>
+        <v-col cols="6">
+          <v-progress-linear
+            color="green accent-4"
+            indeterminate
+            rounded
+            height="6"
+          ></v-progress-linear>
+        </v-col>
+      </v-row>
+    </v-container>
           </v-col>
         </v-row>
       </v-container>
     </div>
   </template>
+  <script>
+export default {
+
+  layout: 'admin',
+  data() {
+    return {
+      logged_in_user:'',
+       jwt_token: '',
+       token_exp: '',
+       oauth_access_token: ''
+    }
+  },
+
+  methods: {
+
+    async getJwt() {
+      
+    //TODO: separar esto para que el login normal tambien pueda usar este metodo
+    this.jwt_token= await this.$axios.$get('http://168.197.48.101:8080/api/users/github/' + this.oauth_access_token)
+    alert(this.jwt_token.message);
+    this.$router.push('/')
+    }
+  },
+
+  mounted() {
+    this.oauth_access_token=this.$route.query.access_token;
+    this.getJwt()
+  },
+  created() {
+    
+  }
+}
+  </script>
+  

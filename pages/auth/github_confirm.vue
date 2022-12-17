08760c23f21fe9e0c04e3edd257115a67dc0eab0
Juan Agustin Fogliato <48728949+juan9889@users.noreply.github.com>
2022-12-17 03:55:23 -0300

    Update github_confirm.vue

--- pages/auth/github_confirm.vue
+++ pages/auth/github_confirm.vue
@@ -37,7 +37,7 @@ export default {
     async getJwt() {
       //TODO: separar esto para que el login normal tambien pueda usar este metodo
       var responseauth = await this.$axios.$get(
-        'http://168.197.48.101:8080/api/users/github/' + this.oauth_access_token
+        'http://168.197.50.238:8080/api/users/github/' + this.oauth_access_token
       )
       this.jwt_token = responseauth.token
       this.user_loggedin = responseauth.data
